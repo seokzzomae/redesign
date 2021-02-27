7a247e9b217d5d771176330f55701c828f7702ae
arkincognito <wonho@yonsei.ac.kr>
2021-02-27 15:14:40 +0900

    Result.kt .setText() -> .text 변경
    코틀린에서 .setText("string") 와 .text = "string"가 하는 일은 같은데,
    .setText()는 자바에서 사용하던 용법이고 .text 가 코틀린스러운 방식이라고 합니다.
    자바에선 TextView 클래스의 변수 text 가 Public String 으로 선언되지 않았으면
    저런 식으로 접근하는게 불가능했을 거예요.
    굳이 바꿀 필요가 없긴 한데 뭔가 코틀린스러운 방식으로 쓰는게 좋지 않을까 해서 바꿨습니다.
    
    MaxScore >= 0 일 때 opinion.setText(situation+"은 안 될것 같아요!")은
    setText 함수 내에서 String concatenation 하면 제대로 표현이 안 될 수 있다고 해서
    따로 변수 만들어서 넣어줬습니다.

--- app/src/main/java/com/example/redesign/Result.kt
+++ app/src/main/java/com/example/redesign/Result.kt
@@ -12,45 +12,31 @@ class Result : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
-
-
         val company1 = intent.getStringExtra("company")   //회사 이름 받음
 
-        resultname.setText(company1)
-
-
+        resultname.text = company1
 
         var situation="관리 조건"  //관리 조건 , 상폐 조건
 
-
-
         var Max_score=25  //가장 안 좋은 점수
-
 
         var text1 ="관련 설명 쓰는 곳곳!!!"
 
         score.progress=Max_score
 
-
-
-
-        detail_opinion.setText(text1)
+        detail_opinion.text = text1
 
         back.setOnClickListener {
 
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
-
         }
-
-
 
         when{
             Max_score >= 75 ->
                 resultImage.setBackgroundResource(
                         R.drawable.bad_result
                 )
-
 
             Max_score > 25 ->
                 resultImage.setBackgroundResource(
@@ -61,44 +47,30 @@ class Result : AppCompatActivity() {
                 resultImage.setBackgroundResource(
                         R.drawable.good_result
                 )
-
         }
 
         when{
             Max_score >= 75 ->
-                yuhumdo.setText("매우 위험")
-
+                yuhumdo.text = "매우 위험"
 
             Max_score > 25 ->
-                yuhumdo.setText("위험")
+                yuhumdo.text = "위험"
 
             Max_score >= 0 ->
-                yuhumdo.setText("양호")
-
+                yuhumdo.text = "양호"
         }
-
 
         when{
             Max_score >= 75 ->
-                opinion.setText("투자를 삼가하세요!")
-
+                opinion.text = "투자를 삼가하세요!"
 
             Max_score > 25 ->
-                opinion.setText("자산이 위험해요!")
+                opinion.text = "자산이 위험해요!"
 
-            Max_score >= 0 ->
-                opinion.setText(situation+"은 안 될것 같아요!")
-
+            Max_score >= 0 -> {
+                val text = situation + "은 안 될것 같아요!"
+                opinion.text = text
+            }
         }
-
-
-
-
-
-
-
-
-
-
     }
 }
