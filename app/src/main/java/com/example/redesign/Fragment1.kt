540bed510d0facdd74d9f8e7db1f4bb41f2a298e
arkincognito <wonho@yonsei.ac.kr>
2021-02-27 14:31:02 +0900

    Fragment1.kt null 처리 변경, onCreateView 변수 할당 없이 바로 return
    1. null 처리 변경
    
    var mainActivity : MainActivity? = null 대신
    lateinit var mainActivity : MainActivity 를 사용해 변수를 미리 선언해주고
    다른 함수에서 변수를 설정하면 null 위험 없이 변수를 사용할 수 있습니다.
    mainActivity 타입이 null 불가능한 MainActivity 로 바뀌었으니
    mainActivity?.는 전부 mainActivity 로 바꿨습니다.
    
    onSaveInstanceState(outState: Bundle){
    ...
    outState?. ....
    여기도 outState 가 null 불가능한 변수라 outState. 로 바꿨습니다.
    
    2. onCreateView 의 변수 view 처럼 선언되고 그대로 리턴되는 값인 경우
    그냥 리턴문에 연산 적으셔도 됩니다.
    저 view 를 가지고 다른 연산을 더 하실 거면 따로 변수를 핧당해야겠지만요.

--- app/src/main/java/com/example/redesign/Fragment1.kt
+++ app/src/main/java/com/example/redesign/Fragment1.kt
@@ -2,17 +2,12 @@ package com.example.redesign
 
 
 import android.content.Context
-import android.content.Intent
 import android.os.Bundle
-import android.os.ParcelFileDescriptor.open
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.frag1.*
-import org.xmlpull.v1.XmlPullParser
-import org.xmlpull.v1.XmlPullParserFactory
-import java.nio.channels.AsynchronousFileChannel.open
 import java.nio.channels.AsynchronousServerSocketChannel.open
 import java.nio.channels.FileChannel.open
 import kotlin.random.Random as Random
@@ -21,31 +16,16 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 
 class Fragment1 : Fragment() {
-
-
-    var mainActivity :MainActivity? =null
+    private lateinit var mainActivity : MainActivity
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
 
-        mainActivity =context as MainActivity
-
-
-
-
+        mainActivity = context as MainActivity
     }
 
-
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle? ): View? {
-        val view =inflater.inflate(R.layout.frag1, container, false)
-
-
-
-
-
-
-        return view
+        return inflater.inflate(R.layout.frag1, container, false)
     }
 
 
@@ -55,20 +35,9 @@ class Fragment1 : Fragment() {
 
         resultButton.setOnClickListener {
 
-            mainActivity?.companyinput()
-
+            mainActivity.companyinput()
         }
-
-
-        mainActivity?.myungun()
-
-
-
-
-
-
-
-
+        mainActivity.myungun()
     }
 
 
@@ -76,12 +45,6 @@ class Fragment1 : Fragment() {
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
 
-        outState?.putString("data1" ,text_myungun.text.toString())
+        outState.putString("data1" ,text_myungun.text.toString())
     }
-
-
-
-
-
-
 }
