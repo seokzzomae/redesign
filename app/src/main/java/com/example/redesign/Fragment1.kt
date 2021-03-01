a407b75047098aef51734ddf203d87ed517b3f8d
arkincognito <wonho@yonsei.ac.kr>
2021-03-01 21:09:14 +0900

    MainActivity 함수 Fragment1으로 이전, 명언 parsing 후 변수로 저장
    Fragment1.kt로 Fragment1 관련 함수 companyInput() 이전
    Fragment1.kt에 명언 xml parsing 해서 ArrayList 로 저장
    랜덤 값 생성해서 Fragment1 뷰 할 때마다 ArrayList 의 랜덤 인덱스 값 접근
    
    TODO :
    1. xml parsing 하는 과정이 여러 번 일어나는지 확인하고
       맞다면 한 번만 하게 바꾸기
    2. 에러 가능 케이스 처리해야함

--- app/src/main/java/com/example/redesign/Fragment1.kt
+++ app/src/main/java/com/example/redesign/Fragment1.kt
@@ -1,50 +1,82 @@
 package com.example.redesign
 
-
-import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.frag1.*
-import java.nio.channels.AsynchronousServerSocketChannel.open
-import java.nio.channels.FileChannel.open
 import kotlin.random.Random as Random
-import kotlinx.android.synthetic.main.activity_main.*
-
+import org.xmlpull.v1.XmlPullParser
+import org.xmlpull.v1.XmlPullParserFactory
 
 
 class Fragment1 : Fragment() {
-    private lateinit var mainActivity : MainActivity
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-
-        mainActivity = context as MainActivity
-    }
+    private lateinit var personArray : ArrayList<String>
+    private lateinit var myungunArray : ArrayList<String>
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle? ): View? {
         return inflater.inflate(R.layout.frag1, container, false)
-
     }
-
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         resultButton.setOnClickListener {
-            mainActivity.companyinput()
+            companyInput()
         }
-        mainActivity.myungun()
+        myungunArray = parseMyungun(1)
+        personArray = parseMyungun(2)
+        setMyungun()
 //        getCorpData 작동하는지 확인하기 위해 추가한 코드. 추후 삭제
 //        mainActivity.getCorpData("anything")
     }
 
-
-
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-
         outState.putString("data1" ,text_myungun.text.toString())
+    }
+
+    // 명언 저장된 xml 파일을 nameArray, myungunArray 변수로 저장
+    private fun parseMyungun(attributeIndex : Int) : ArrayList<String>{
+        // attributeIndex 1 -> 명언
+        // attributeIndex 2 -> 이름
+        val myungunArray = arrayListOf<String>()
+
+        val xmlData = context!!.assets.open("file1.xml")
+        val factory = XmlPullParserFactory.newInstance()
+        val parser = factory.newPullParser()
+
+        parser.setInput(xmlData, null)
+
+        var event = parser.eventType
+        while (event != XmlPullParser.END_DOCUMENT) {
+            val tagName = parser.name
+
+            when (event) {
+                XmlPullParser.END_TAG -> {
+                    if (tagName == "text") {
+                        val attribute = parser.getAttributeValue(attributeIndex)
+                        myungunArray.add(attribute)
+                    }
+                }
+            }
+            event = parser.next()
+        }
+        return myungunArray
+    }
+
+    // 명언 수에서 저장된 명언과 이름 어레이에서
+    private fun setMyungun(){
+        val index = Random.nextInt(MainActivity.NUM_MYUNGUN)
+        text_myungun.text = myungunArray[index]
+        text_person.text = personArray[index]
+    }
+
+    private fun companyInput() {
+        // TODO 회사명 없이 검색 눌렀을 때 검색 넘어가지 않게, 다른 에러 가능한 케이스도 처리해줘야함
+        val intent = Intent(context, Result::class.java)
+        intent.putExtra("company",companyname.text.toString())
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/redesign/MainActivity.kt
+++ app/src/main/java/com/example/redesign/MainActivity.kt
@@ -1,23 +1,12 @@
 package com.example.redesign
 
-import android.content.Context
-import android.content.Intent
 import android.content.res.AssetManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.navigation.NavigationView
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.frag1.*
-import org.xmlpull.v1.XmlPullParser
-import org.xmlpull.v1.XmlPullParserFactory
-import kotlin.random.Random as Random
 import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONObject
 
@@ -31,7 +20,6 @@ class MainActivity : AppCompatActivity() , NavigationView.OnNavigationItemSelect
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         setFrag(0)
 
         btn_analyze.setOnClickListener {
@@ -42,32 +30,25 @@ class MainActivity : AppCompatActivity() , NavigationView.OnNavigationItemSelect
             setFrag(1)
         }
 
-
         btn_navi.setOnClickListener{
             layout_drawer.openDrawer(GravityCompat.START)
         }
         naviView.setNavigationItemSelectedListener(this)
     }
 
-
-
     private fun setFrag(fragNum: Int) {
         val ft = supportFragmentManager.beginTransaction()
         when(fragNum)
         {
-            0 ->{
+            0 -> {
                 ft.replace(R.id.main_frame, Fragment1()).commit()
             }
 
-            1 ->{
+            1 -> {
                 ft.replace(R.id.main_frame, Fragment2()).commit()
             }
         }
     }
-
-    
-
-
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean { //네비게이션 메뉴 아이템 클릭 시 수행
         when (item.itemId)
@@ -76,45 +57,8 @@ class MainActivity : AppCompatActivity() , NavigationView.OnNavigationItemSelect
             R.id.email -> Toast.makeText(applicationContext,"이메일",Toast.LENGTH_SHORT).show()
             R.id.message -> Toast.makeText(applicationContext,"메세지",Toast.LENGTH_SHORT).show()
         }
-
-
         layout_drawer.closeDrawers()
-
         return false
-    }
-
-
-    fun myungun() {
-        // getAttributeValue로 현재 인덱스 값을 랜덤 값과 비교하면서 next()로 넘기지 말고
-        // 그냥 첫 "text" 태그 나오면 랜덤 값 횟수만큼 next() 해서 명언 찾기로 바꾸는게 연산 숫자 덜 들 것 같습니다.
-
-        // TODO 현재 이 코드에 문제가 있는 것 같습니다. 수정이 필요할 것 같네요.
-        // 일단 지금 당장 Merge 하게 되면 석호TL님이 작업하신 걸로 사용해야합니다
-
-        val xml_data = assets.open("file1.xml")
-        val factory =XmlPullParserFactory.newInstance()
-        val parser =factory.newPullParser()
-
-        // 명언 수가 바뀌면 명언 인덱스 범위가 바뀔테니 변수처리 하였습니다
-        // Random.nextInt()는 변수가 하나 들어가면 0~변수 -1 까지의 수를 랜덤으로 내놓기 때문에
-        // 총 명언 개수를 넣어줘야합니다
-        val index =Random.nextInt(NUM_MYUNGUN)
-
-        parser.setInput(xml_data,null)
-        var event = parser.eventType
-
-        // 첫번째 명언이 나올 때까지 스킵(parser.name : xml_tag 값 반환)
-        while (parser.name != "text") {event = parser.next()}
-
-        // 랜덤 인덱스 값만큼 명언 스킵
-        for (i in 0..index-1) {event = parser.next()}
-
-        // 랜덤 인덱스 값의 명언에 도달했으므로 명언 내용과 명언을 남긴 사람의 이름을 저장하고 반환
-        val myungun ="\n" +parser.getAttributeValue(1)
-        text_myungun.text = myungun
-
-        val name ="\n" +parser.getAttributeValue(2)
-        text_person.text = name
     }
 
     fun getCorpData(corp_name: String) {
@@ -124,14 +68,6 @@ class MainActivity : AppCompatActivity() , NavigationView.OnNavigationItemSelect
         val jObject = JSONObject(jsonString)
         val jArray = jObject.getJSONArray("rprts")
         val investEvaluator = InvestEvaluator(jArray, corp_name)
-    }
-
-
-    fun companyinput() {
-        // TODO 회사명 없이 검색 눌렀을 때 검색 넘어가지 않게, 다른 에러 가능한 케이스도 처리해줘야함
-        val intent = Intent(this,Result::class.java)
-        intent.putExtra("company",companyname.text.toString())
-        startActivity(intent)
     }
 }
 
